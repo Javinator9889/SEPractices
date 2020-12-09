ff88669c7ffd99c8074eb79970328c8494fb8b8d
Javinator9889 <dev@javinator9889.com>
2020-12-09 19:22:46 +0100

    orchestrator: initial version

---
+++ calculator/calculator.srcs/sources_1/new/ButtonIf.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: NPCMOS
+-- Engineers: Javinator9889 & M. Octavian
+-- 
+-- Create Date: 09.12.2020 17:31:43
+-- Design Name: 
+-- Module Name: ButtonIf - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision: 0.01
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ButtonIf is
+    Port ( btn_sum: in std_logic;
+           btn_sub: in std_logic;
+           btn_mul: in std_logic;
+           btn_div: in std_logic;
+           btn_eq:  in std_logic;
+           result:  inout std_logic_vector(3 downto 0);
+           execute: inout std_logic
+    );
+end ButtonIf;
+
+architecture Behavioral of ButtonIf is
+
+begin
+
+    process (btn_sum)
+    begin
+        if btn_sum = '1' then
+            result <= "0000";
+        end if;
+    end process;
+    
+    process (btn_sum)
+    begin
+        if btn_sum = '1' then
+            result <= "0001";
+        end if;
+    end process;
+    
+    process (btn_mul)
+    begin
+        if btn_sum = '1' then
+            result <= "0010";
+        end if;
+    end process;
+    
+    process (btn_div)
+    begin
+        if btn_sum = '1' then
+            result <= "0011";
+        end if;
+    end process;
+    
+    process (btn_eq)
+    begin
+        if btn_eq = '1' then
+            execute <= '1';
+        end if;
+    end process;
+
+end Behavioral;
--- calculator/calculator.srcs/sources_1/new/Orchestrator.vhd
+++ calculator/calculator.srcs/sources_1/new/Orchestrator.vhd
@@ -26,6 +26,7 @@ use IEEE.NUMERIC_STD.ALL;
 use WORK.ALU;
 use WORK.Registry;
 use WORK.SwitchIf;
+use WORK.ButtonIf;
 use WORK.Constants.ALL;
 use WORK.BinaryUtils.ALL;
 
@@ -76,6 +77,19 @@ component switchif is
     );
 end component;
 
+-- Component ButtonIf declaration
+component buttonif is
+    port (
+        btn_sum: in std_logic;
+        btn_sub: in std_logic;
+        btn_mul: in std_logic;
+        btn_div: in std_logic;
+        btn_eq:  in std_logic;
+        result:  inout std_logic_vector(3 downto 0);
+        execute: inout std_logic
+    );
+end component;
+
 -- ALU signals
 signal ALU_a        : std_logic_vector (MSB downto LSB);
 signal ALU_b        : std_logic_vector (MSB downto LSB);
@@ -87,7 +101,116 @@ signal ALU_carry    : std_logic;
 -- Switch signals
 signal sw           : std_logic_vector (MSB downto LSB);
 
+-- Button signals
+signal btn_sum      : std_logic;
+signal btn_sub      : std_logic;
+signal btn_mul      : std_logic;
+signal btn_div      : std_logic;
+signal btn_eq       : std_logic;
+signal result       : std_logic_vector(3 downto 0);
+signal execute      : std_logic;
+
+-- First operand signals
+signal f1_write     : std_logic;
+signal f1_clr       : std_logic;
+signal f1_d         : std_logic_vector(MSB downto LSB);
+signal f1_q         : std_logic_vector(MSB downto LSB);
+
+-- Second operand signals
+signal f2_write     : std_logic;
+signal f2_clr       : std_logic;
+signal f2_d         : std_logic_vector(MSB downto LSB);
+signal f2_q         : std_logic_vector(MSB downto LSB);
+
+-- Operand selection signal
+signal operand_sel  : std_logic_vector(2 downto 0);
+
+-- AC button counter
+signal ac_counter   : natural;
+
 begin
+
+    sys_alu: ALU port map (ALU_a => ALU_a,
+                           ALU_b => ALU_b,
+                           ALU_sel => ALU_sel,
+                           ALU_go => ALU_go,
+                           ALU_out => ALU_out,
+                           ALU_carry => ALU_carry);
+                          
+    first_operand: registry port map(clk => clk,
+                                     write => f1_write,
+                                     clr => f1_clr,
+                                     d => f1_d,
+                                     q => f1_q);
+                                     
+    second_operand: registry port map(clk => clk,
+                                      write => f2_write,
+                                      clr => f2_clr,
+                                      d => f2_d,
+                                      q => f2_q);
+                                      
+    button_selection: buttonif port map(btn_sum => btn_sum,
+                                        btn_sub => btn_sub,
+                                        btn_mul => btn_mul,
+                                        btn_div => btn_div,
+                                        btn_eq => btn_eq,
+                                        result => result,
+                                        execute => execute);
+                                        
+    operand_selection: switchif port map(sw => sw);
+    
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            case (operand_sel) is
+                -- First operand data
+                when "00" => 
+                    f1_d <= bcd16_to_binary(sw);
+                    f1_write <= '1';
+                    if execute = '1' then
+                        ALU_a <= f1_q;
+                        execute <= '0';
+                        operand_sel <= "01";
+                    end if;
+                -- Second operand data
+                when "01" =>
+                    f2_d <= bcd16_to_binary(sw);
+                    f2_write <= '1';
+                    if execute = '1' then
+                        ALU_b <= f2_q;
+                        execute <= '0';
+                        operand_sel <= "10";
+                    end if;
+            end case;
+        end if;
+    end process;
+    
+    process (clk, execute)
+    begin
+        if rising_edge(clk) and execute = '1' then
+            case (operand_sel) is
+                when "01" =>
+                    ALU_b <= f2_q;
+                    ALU_go <= '1';
+                    operand_sel <= "10";
+                when "10" =>
+                    ac_counter <= ac_counter + 1;
+                    if ac_counter = 2 then
+                        operand_sel <= "00";
+                        ac_counter <= 0;
+                    end if;
+            end case;
+        end if;
+    end process;
+    
+    process (result)
+    begin
+        if operand_sel = "00" then
+            ALU_a <= f1_q;
+            ALU_sel <= result;
+            operand_sel <= "01";
+        end if;
+    end process;
 
 
 end Behavioral;
